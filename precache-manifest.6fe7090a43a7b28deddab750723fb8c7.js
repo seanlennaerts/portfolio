b1ef6e1cc292ea549a0319efbeeb97ff3a77ff0e
Sean Lennaerts <seanlenny@gmail.com>
2018-11-16 19:58:33 -0800

    Updates

---
+++ precache-manifest.6fe7090a43a7b28deddab750723fb8c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c6b2cafaed2a7b843cc",
+    "url": "/static/js/main.9c6b2caf.chunk.js"
+  },
+  {
+    "revision": "4ee59bd3a53a8d8540cd",
+    "url": "/static/js/1.4ee59bd3.chunk.js"
+  },
+  {
+    "revision": "9c6b2cafaed2a7b843cc",
+    "url": "/static/css/main.5b038fad.chunk.css"
+  },
+  {
+    "revision": "d04c7d80a894247e3d34fcf51a0eeedf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2ac28d1893d1658e90c4bc4a1fae2c8.js"
+  "/precache-manifest.6fe7090a43a7b28deddab750723fb8c7.js"
 );
 
 workbox.clientsClaim();
